f656746a6555a6e1adbfa288a7721356aaace432
luke weber <lukedriftingweber@gmail.com>
2022-01-06 15:14:24 +1030

    questions added and code check

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,40 @@ var question = document.querySelector(".question")
 var initials = document.querySelector(".initials")
 var highscores = document.querySelector(".highscores")
 var startquiz = document.querySelector("#start-quiz")
+var questionarray = [{
+    title:"Commonly used data types DO NOT include", 
+    choices:["Strings", "Booleans", "Alerts","Numbers"],
+    answer:"Alerts"
+},{
+    title:"Commonly used data types DO NOT include", 
+    choices:["Strings", "Booleans", "Alerts","Numbers"],
+    answer:"Alerts"  
+},{
+    title:"Commonly used data types DO NOT include", 
+    choices:["Strings", "Booleans", "Alerts","Numbers"],
+    answer:"Alerts"
+},{
+    title:"Commonly used data types DO NOT include", 
+    choices:["Strings", "Booleans", "Alerts","Numbers"],
+    answer:"Alerts"
+},{
+    title:"Commonly used data types DO NOT include", 
+    choices:["Strings", "Booleans", "Alerts","Numbers"],
+    answer:"Alerts"
+}]
 
 startquiz.addEventListener("click" ,function(){
     intro.classList.add("hide")
     question.classList.remove("hide")
+    displayQuestion()
 })
+function displayQuestion(){
+    question.innerHTML=`
+    <h3>${questionarray[0].title}</h3>
+        <ol>
+            <li><button class="choices">${questionarray[0].choices[0]}</button></li>
+            <li><button class="choices">${querySelector[0].choices[1]}</button></li>
+            <li><button class="choices">${}</button></li>
+            <li><button class="choices">Numbers</button></li>
+        </ol>`
+}
